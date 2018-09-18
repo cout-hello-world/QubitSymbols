bdc1960e41cf5fadca633b139364f66a71e94d45
Henry Elliott <henrylovesscience@gmail.com>
2018-09-17 23:44:07 -0500

    Add code for drawing qubit systems

--- draw.cc
+++ draw.cc
@@ -6,62 +6,86 @@
 
 #include "pair.hh"
 
+// A   B
+//
+// C   D
+//
+// E   F
+
 namespace {
+constexpr auto A = "(0,2)";
+constexpr auto B = "(1,2)";
+constexpr auto C = "(0,1)";
+constexpr auto D = "(1,1)";
+constexpr auto E = "(0,0)";
+constexpr auto F = "(1,0)";
+
+constexpr auto header = "\\documentclass[letterpaper]{article}\n"
+"\\usepackage{tikz}\n\\usepackage[margin=1in]{geometry}\n\\begin{document}\n"
+"\\noindent\n";
+constexpr auto footer = "\\end{document}\n";
+
+std::string drawstr(const std::string &begin, const std::string &end) {
+	return "\\draw " + begin + " -- " + end + ";\n";
+}
+
 void print_state(std::ostream &out, std::uint16_t state) {
-	std::string p = "{";
+	std::string p = "\\begin{tikzpicture}\n";
 	if (state & AB) {
-		p += "{A,B},";
+		p += drawstr(A, B);
 	}
 	if (state & AC) {
-		p += "{A,C},";
+		p += drawstr(A, C);
 	}
 	if (state & AD) {
-		p += "{A,D},";
+		p += drawstr(A, D);
 	}
 	if (state & AE) {
-		p += "{A,E},";
+		p += drawstr(A, E);
 	}
 	if (state & AF) {
-		p += "{A,F},";
+		p += drawstr(A, F);
 	}
 	if (state & BC) {
-		p += "{B,C},";
+		p += drawstr(B, C);
 	}
 	if (state & BD) {
-		p += "{B,D},";
+		p += drawstr(B, D);
 	}
 	if (state & BE) {
-		p += "{B,E},";
+		p += drawstr(B, E);
 	}
 	if (state & BF) {
-		p += "{B,F},";
+		p += drawstr(B, F);
 	}
 	if (state & CD) {
-		p += "{C,D},";
+		p += drawstr(C, D);
 	}
 	if (state & CE) {
-		p += "{C,E},";
+		p += drawstr(C, E);
 	}
 	if (state & CF) {
-		p += "{C,F},";
+		p += drawstr(C, F);
 	}
 	if (state & DE) {
-		p += "{D,E},";
+		p += drawstr(D, E);
 	}
 	if (state & DF) {
-		p += "{D,F},";
+		p += drawstr(D, F);
 	}
 	if (state & EF) {
-		p += "{E,F},";
+		p += drawstr(E, F);
 	}
-	p[p.size() - 1] = '}';
-	out << p << '\n';
+	p += "\\end{tikzpicture}\n";
+	out << p;
 }
 }
 
 void draw(std::ostream &out, const std::unordered_set<std::uint16_t> &states)
 {
+	out << header;
 	for (auto state : states) {
 		print_state(out, state);
 	}
+	out << footer;
 }
